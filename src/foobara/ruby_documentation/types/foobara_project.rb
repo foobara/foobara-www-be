43bb71aad7fc98233cbed916a260ac4f0302114b
Miles Georgi <azimux@gmail.com>
2025-08-22 18:18:08 -0700

    Generate latest build version of docs for each minor version

--- src/foobara/ruby_documentation/types/foobara_project.rb
+++ src/foobara/ruby_documentation/types/foobara_project.rb
@@ -13,6 +13,27 @@ module Foobara
       def latest_version
         versions.first
       end
+
+      def latest_build_versions
+        @latest_build_versions ||= begin
+          numeric_versions = versions.map do |version|
+            version.split(".").map(&:to_i)
+          end
+
+          by_minor = {}
+
+          numeric_versions.each do |version|
+            minor = [version[0], version[1]]
+
+            by_minor[minor] ||= []
+            by_minor[minor] << version
+          end
+
+          by_minor.values.map(&:max).map do |numeric_version|
+            numeric_version.join(".")
+          end
+        end
+      end
     end
   end
 end
